d2bbc0a3a027669b7aeff1ff82eac55b84154bc2
petedyerarm <pete.dyer@izumanetworks.com>
2023-04-19 20:32:25 +0100

    sanity

--- led_flash_app_socket/led_flash.py
+++ led_flash_app_socket/led_flash.py
@@ -3,37 +3,38 @@
 import sys
 import time
 
+
 def red(on):
-    with open('/work/led1/brightness', 'w') as red:
+    with open("/work/led1/brightness", "w") as red:
         if on:
             print("RED: ON")
-            red.write('1')
+            red.write("1")
         else:
             print("RED: OFF")
-            red.write('0')
+            red.write("0")
+
 
 def green(on):
-    with open('/work/led0/brightness', 'w') as green:
+    with open("/work/led0/brightness", "w") as green:
         if on:
             print("GRN: ON")
-            green.write('1')
+            green.write("1")
         else:
             print("GRN: OFF")
-            green.write('0')
+            green.write("0")
+
 
 def init_leds():
-    with open('/work/led1/trigger', 'w') as red_trigger:
-        red_trigger.write('gpio')
+    with open("/work/led1/trigger", "w") as red_trigger:
+        red_trigger.write("gpio")
     red(False)
-    with open('/work/led0/trigger', 'w') as green_trigger:
-        green_trigger.write('gpio')
+    with open("/work/led0/trigger", "w") as green_trigger:
+        green_trigger.write("gpio")
     green(False)
 
 
 def main():
-
     init_leds()
-    time.sleep(1)
 
     red(True)
     green(False)
@@ -54,4 +55,3 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
-

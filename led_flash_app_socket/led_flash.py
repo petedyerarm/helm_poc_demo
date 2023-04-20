86df24f688b9d974465569d86bf78a8c8b5795c1
petedyerarm <pete.dyer@izumanetworks.com>
2023-04-20 13:12:52 +0100

    sanity

--- led_flash_app_socket/led_flash.py
+++ led_flash_app_socket/led_flash.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 
+"""Flash the LEDs on a Raspberry Pi."""
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023, Izuma Networks and affiliates.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
+
 import argparse
 import sys
 import time
+
+
+green_off = False
+red_off = False
+red_seconds = options.red_on
+green_seconds = options.green_on
 
 
 def _red(on):
@@ -59,11 +85,6 @@ def main():
     print(options.red_off)
 
     _init_leds()
-
-    green_off = False
-    red_off = False
-    red_seconds = options.red_on
-    green_seconds = options.green_on
 
     while True:
         if green_off:

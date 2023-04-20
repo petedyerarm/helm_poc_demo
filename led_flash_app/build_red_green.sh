3f7a2ae48def3d7b39b1a91c172c6da7f2a0c237
petedyerarm <pete.dyer@izumanetworks.com>
2023-04-20 16:52:57 +0100

    Added containers to flash either the RED or the GREEN led, and updated the helm notes to display this behaviour.

---
+++ led_flash_app/build_red_green.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Parse command line
+args_list="account:"
+args_list="api_key:"
+args_list="${args_list},verbose"
+
+args=$(getopt -o+ho:x $args_list -n "$(basename "$0")" -- "$@")
+
+eval set -- "$args"
+
+while [ $# -gt 0 ]; do
+    if [ -n "${opt_prev:-}" ]; then
+        eval "$opt_prev=\$1"
+        opt_prev=
+        shift 1
+        continue
+    elif [ -n "${opt_append:-}" ]; then
+        eval "$opt_append=\"\${$opt_append:-} \$1\""
+        opt_append=
+        shift 1
+        continue
+    fi
+    case $1 in
+    --account)
+        opt_prev=accountId
+        ;;
+    --api_key)
+        opt_prev=apiKey
+        ;;
+    --verbose)
+        set -x
+        ;;
+    esac
+    shift 1
+done
+
+if [ -z "${accountId:-}" ]; then
+    echo "Account ID not specified"
+    exit 1
+fi
+
+if [ -z "${apiKey:-}" ]; then
+    echo "API Key not specified"
+    exit 1
+fi
+
+
+docker login containers.us-east-1.mbedcloud.com -u KEY -p  "${apiKey}"
+
+
+docker build -t containers.us-east-1.mbedcloud.com/"${accountId}"/led_poc_demo:red -f Dockerfile-red .
+docker push  containers.us-east-1.mbedcloud.com/"${accountId}"/led_poc_demo:red
+
+docker build -t containers.us-east-1.mbedcloud.com/"${accountId}"/led_poc_demo:green -f Dockerfile-green .
+docker push  containers.us-east-1.mbedcloud.com/"${accountId}"/led_poc_demo:green
+
+
+curl https://containers.us-east-1.mbedcloud.com/v2/"${accountId}"/led_poc_demo/tags/list -H "Authorization: Bearer ${apiKey}"
+
+
+

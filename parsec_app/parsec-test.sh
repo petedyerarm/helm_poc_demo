345cc0a450477e2455226d41183376d685de959d
petedyerarm <pete.dyer@izumanetworks.com>
2023-04-21 12:48:49 +0100

    Tidy.

--- parsec_app/parsec-test.sh
+++ parsec_app/parsec-test.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
-printf "Running as `id`\n\n"
+printf "Running as %s\n\n" "$(id)"
 
-if /usr/src/app/parsec-tool ping | grep '1.0'; then 
+if parsec-tool ping | grep '1.0'; then
     printf "Parsec service is reachable!\n\n"
 
     printf "List the keys managed by this user..\n\n"
-    /usr/src/app/parsec-tool list-keys
-    if /usr/src/app/parsec-tool list-keys |& grep "No keys currently available"; then
+    parsec-tool list-keys
+    if parsec-tool list-keys |& grep "No keys currently available"; then
         # No keys found in TPM which are either created or accessed by this user
         # Create ECC key pair
         printf "Create an ECC key pair..\n\n"
-        /usr/src/app/parsec-tool create-ecc-key --key-name `id -u`-key-$((1 + $RANDOM % 10))
-        /usr/src/app/parsec-tool list-keys
+        parsec-tool create-ecc-key --key-name "$(id -u)"-key-$((1 + RANDOM % 10))
+        parsec-tool list-keys
     fi
-    /usr/src/app/parsec-tool export-public-key --key-name $(/usr/src/app/parsec-tool  list-keys | cut -d" " -f2)
+    parsec-tool export-public-key --key-name "$(parsec-tool  list-keys | cut -d" " -f2)"
 fi
 
 
